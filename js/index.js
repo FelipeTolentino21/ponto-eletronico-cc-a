3fd19909b36eddf4628c7073300d8a5b79f12912
FelipeTolentino21 <tolentinofelipe21@gmail.com>
2024-09-02 11:02:55 -0300

    Alterações gerais em todos os arquivos, adição de um dialog e coisas relacionadas a ele

--- js/index.js
+++ js/index.js
@@ -1,29 +1,75 @@
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+});
+
+
+// Constantes para definir os elementos 'de horário' e o botão de 'registrar o ponto'
 const diaSemana = document.getElementById("dia-semana");
 const dataAtual = document.getElementById("data-atual");
 const horaAtual = document.getElementById("hora-atual");
 const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
 
-btnRegistrarPonto.addEventListener("click", registrar);
+// Ao clicar no "btnRegistrarPonto" a função "registrar" é chamada
+btnRegistrarPonto.addEventListener("click", register);
 
+// Atualização dos textos "dataAtual" e "diaSemana" chamando as funções "getCurrentDate" e "getWeekDay"
 dataAtual.textContent = getCurrentDate();
 diaSemana.textContent = getWeekDay();
 
 
-function registrar(){
-    alert("botao ponto!");
+
+// Constante definindo o dialog "dialog-ponto"
+const dialogPonto = document.getElementById("dialog-ponto");
+
+// Constante definindo o botão "dialog-fechar"
+const btnDialogFechar = document.getElementById("dialog-fechar");
+
+// Ao clicar no "btnDialogFechar" a função anônima fecha o "dialogPonto"
+btnDialogFechar.addEventListener("click", () => {
+    dialogPonto.close();
+});
+
+// Constantes para os texots "dialog-data" e "dialog-hora"
+const dialogData = document.getElementById("dialog-data");
+const dialogHora = document.getElementById("dialog-hora");
+
+// "dialogData" e "dialogHora" chamando duas funções para receber a data e hora atual respectivamente
+dialogData.textContent = getCurrentDate();
+dialogHora.textContent = getCurrentTime();
+
+
+const btnDialogEntrada = document.getElementById("btn-dialog-entrada");
+const btnDialogSaida = document.getElementById("btn-dialog-saida");
+
+btnDialogEntrada.addEventListener("click", dialogRegister);
+btnDialogSaida.addEventListener("click", dialogRegister);
+
+function dialogRegister(){
+    console.log(getCurrentDate());
+    console.log(getCurrentTime());
 }
 
+
+
+// Função registrar abre o "dialogPonto"
+function register(){
+    dialogPonto.showModal();
+}
+// Função que atualiza o texto "horaAtual" e "dialogHora" com o tempo que a função "getCurrentTime" retorna
 function updateContentHour(){
     horaAtual.textContent = getCurrentTime();
+    dialogHora.textContent = getCurrentTime();
 }
 
-// Retorna a hora atual (hora:minuto:segundo)
+// Função que retorna a hora atual (hora:minuto:segundo)
 function getCurrentTime(){
     const date = new Date();  
     return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
 }
 
-// Retorna a data atul (dd/mm/aaaa)
+// Função que retorna a data atul (dd/mm/aaaa)
 function getCurrentDate(){
     const date = new Date();
 
@@ -32,7 +78,7 @@ function getCurrentDate(){
     return String(date.getDate()).padStart(2, '0') + "/" + String(mes).padStart(2, '0') + "/" + date.getFullYear();
 }
 
-// Retorna o dia da semana (De domingo a sábado / 0 - 6)
+// Função que retorna o dia da semana (De domingo a sábado / 0 - 6)
 function getWeekDay(){
     const date = new Date();
 
@@ -41,12 +87,22 @@ function getWeekDay(){
     return semana[dia];
 }
 
+// Chamada da função "updateContentHour" para atualizar instantânemante o horário no site
 updateContentHour();
+
+// Função que atuliza a função "updateContentHour" a cada 1000 milisegundos
 setInterval(updateContentHour, 1000);
 
+
+// Console logs para mostrar o dia da semana, a data atual e as horas no console
 console.log(getWeekDay());
 console.log(getCurrentDate());
 console.log(getCurrentTime());
 
+
+
 // A fazer, atualizar a data e o dia da semana se o usuário bater o ponto meia noite
-// A fazer 2, usar <dialog> para criar um popup quando se é clicado no botão "Registrar ponto" 
+// A fazer 2, usar <dialog> para criar um popup quando se é clicado no botão "Registrar ponto"
+// A fazer 3, formatar a data dependendo do local onde o site é acessado
+
+// btnDialogEntrada que recupera as informações (data, hora, localização [latitude, longitude], tipo: entrada) e salvar essas infos num objeto JavaScript
