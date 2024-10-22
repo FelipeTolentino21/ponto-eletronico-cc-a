eecbf9c69a07d3d9ab26ef120617550a8dd395df
FelipeTolentino <felipe.tolentino@sempreceub.com>
2024-10-22 17:34:55 -0300

    Alterações no index.html, mudança nos estilos do ponto anterior, imagens adicionadas, mudanças no JS, adição do test.js para testar coisas.

--- js/test.js
+++ js/test.js
@@ -1,69 +1,73 @@
-// Constantes para definir os elementos 'de horário' e o botão de 'registrar o ponto'
+// Obtém os elementos HTML referentes ao dia da semana, data, hora e botão de registrar ponto
 const diaSemana = document.getElementById("dia-semana");
 const dataAtual = document.getElementById("data-atual");
 const horaAtual = document.getElementById("hora-atual");
 const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
 
-// Ao clicar no "btnRegistrarPonto" a função "registrar" é chamada
+// Adiciona um evento de clique no botão "Registrar Ponto", chamando a função "register"
 btnRegistrarPonto.addEventListener("click", register);
 
-// Atualização dos textos "dataAtual" e "diaSemana" chamando as funções "getCurrentDate" e "getWeekDay"
+// Atualiza o conteúdo de "dataAtual" e "diaSemana" com os valores retornados pelas funções "getCurrentDate" e "getWeekDay"
 dataAtual.textContent = "Data: " + getCurrentDate();
 diaSemana.textContent = getWeekDay();
 
-// Constante definindo o dialog "dialog-ponto"
+// Obtém o elemento dialog do ponto
 const dialogPonto = document.getElementById("dialog-ponto");
 
-// Constante definindo o botão "dialog-fechar"
+// Obtém o botão para fechar o dialog do ponto
 const btnDialogFechar = document.getElementById("dialog-fechar");
 
-// Ao clicar no "btnDialogFechar" a função anônima fecha o "dialogPonto"
+// Adiciona um evento de clique no botão de fechar o dialog, fechando os dialogs "dialogPonto" e "pontoAnterior"
 btnDialogFechar.addEventListener("click", () => {
     dialogPonto.close();
+    pontoAnterior.close();
 });
 
-// Constantes para os texots "dialog-data" e "dialog-hora"
+// Obtém os elementos de texto referentes à data e hora no dialog
 const dialogData = document.getElementById("dialog-data");
 const dialogHora = document.getElementById("dialog-hora");
 
-// "dialogData" e "dialogHora" chamando duas funções para receber a data e hora atual respectivamente
+// Atualiza os elementos "dialogData" e "dialogHora" com a data e hora atual
 dialogData.textContent = getCurrentDate();
 dialogHora.textContent = getCurrentTime();
 
-// Apresentar para o usuário o valor correspondente ao provável tipo de ponto
-// EX. se o último ponto do usuário for do tipo entrada, selecionar por padrão a option intervalo
+// Obtém o elemento select que permite selecionar o tipo de registro (entrada, intervalo, etc.)
 const selectRegisterType = document.getElementById("register-type");
 
-function setRegisterType(){
+// Define o tipo de registro com base no último registro feito pelo usuário
+function setRegisterType() {
     let lastType = localStorage.setItem("lastRegisterType");
+    
+    // Define o próximo tipo de registro com base no último
     if(lastType == "entrada") {
         selectRegisterType.value = "intervalo";
     }
     if(lastType == "intervalo") {
-        selectRegisterType.value = "volta-intervalo"
+        selectRegisterType.value = "volta-intervalo";
     }
     if(lastType == "volta-intervalo") {
-        selectRegisterType.value = "saida"
+        selectRegisterType.value = "saida";
     }
     if(lastType == "saida") {
-        selectRegisterType.value = "entrada"
+        selectRegisterType.value = "entrada";
     }
-
+    
+    // Atualiza a hora no dialog
     dialogHora.textContent = "Hora: " + getCurrentTime();
 
+    // Atualiza a hora no dialog a cada segundo
     let interval = setInterval(() => {
         dialogHora.textContent = "Hora: " + getCurrentTime();
     }, 1000);
-
 }
 
-// Função registrar abre o "dialogPonto"
-function register(){
-    
+// Função para abrir o dialog de registro de ponto
+function register() {
     const dialogUltimoRegistro = document.getElementById("dialog-ultimo-registro");
     let lastRegister = JSON.parse(localStorage.getItem("lastRegister"));
 
-    if (lastRegister){
+    // Se houver um último registro, exibe suas informações
+    if (lastRegister) {
         let lastDateRegister = lastRegister.date;
         let lastTimeRegister = lastRegister.time;
         let lastRegisterType = lastRegister.type;
@@ -71,23 +75,25 @@ function register(){
         dialogUltimoRegistro.textContent = "Último registro: " + lastDateRegister + " às " + lastTimeRegister + " durante o registro " + lastRegisterType;
     }
 
+    // Esconde o alerta de sucesso, caso esteja visível
     const alertaSucesso = document.getElementById("alerta-ponto-registrado");
     alertaSucesso.classList.remove("show");
     alertaSucesso.classList.add("hidden");
 
-    clearTimeout();
-
+    // Abre o dialog de registro de ponto
     dialogPonto.showModal();
 }
 
+// Adiciona um evento ao botão de registrar no dialog para salvar o registro
 btnDialogRegister = document.getElementById("btn-dialog-register");
-btnDialogRegister.addEventListener("click", async ()=>{
-
+btnDialogRegister.addEventListener("click", async () => {
     let register = await getObjetctRegister(selectRegisterType.value);
+
+    // Salva o registro no localStorage
     saveRegisterLocalStorage(register);
+    localStorage.setItem("lastRegister", JSON.stringify(register));
 
-    localStorage.setItem("lastRegister", JSON.stringify(register))
-
+    // Mostra o alerta de sucesso por 5 segundos
     const alertaSucesso = document.getElementById("alerta-ponto-registrado");
     alertaSucesso.classList.remove("hidden");
     alertaSucesso.classList.add("show");
@@ -97,109 +103,103 @@ btnDialogRegister.addEventListener("click", async ()=>{
         alertaSucesso.classList.add("hidden");
     }, 5000);
 
+    // Fecha o dialog de registro de ponto
     dialogPonto.close();
-})
+});
 
-
-function getUserLocation(){
+// Função para obter a localização do usuário
+function getUserLocation() {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition((position) => {
             let userLocation = {
                 "latitude": position.coords.latitude,
                 "longitude": position.coords.longitude
-            }
+            };
             resolve(userLocation);
-        }, 
-        (error) => {
+        }, (error) => {
             reject("Erro " + error);
         });
     });
 }
 
-async function getObjetctRegister(registerType){
-
+// Função para criar o objeto de registro com as informações necessárias
+async function getObjetctRegister(registerType) {
     const location = await getUserLocation();
 
-    console.log(location);
-
-    ponto = {
+    // Cria o objeto com a data, hora, localização, id e tipo de registro
+    let ponto = {
         "date": getCurrentDate(),
         "time": getCurrentTime(),
         "location": location,
         "id": 1,
         "type": registerType
-    }
+    };
 
     return ponto;
 }
 
+// Obtém os registros do localStorage
 let registerLocalStorage = getRegisterLocalStorage("register");
 
-function saveRegisterLocalStorage(register){
+// Função para salvar o registro no localStorage
+function saveRegisterLocalStorage(register) {
     registerLocalStorage.push(register);
-
     localStorage.setItem("register", JSON.stringify(registerLocalStorage));
 }
 
-function getRegisterLocalStorage(key){
+// Função para obter os registros do localStorage a partir de uma chave específica
+function getRegisterLocalStorage(key) {
     let register = localStorage.getItem(key);
 
-    if(!register) {
+    // Se não houver registros, retorna um array vazio
+    if (!register) {
         return [];
     }
     return JSON.parse(register);
 }
 
-
-// Função que atualiza o texto "horaAtual" e "dialogHora" com o tempo que a função "getCurrentTime" retorna
-function updateContentHour(){
+// Atualiza o conteúdo de "horaAtual" e "dialogHora" com a hora atual
+function updateContentHour() {
     horaAtual.textContent = getCurrentTime();
     dialogHora.textContent = getCurrentTime();
 }
 
-// Função que retorna a hora atual (hora:minuto:segundo)
-function getCurrentTime(){
-    const date = new Date();  
+// Retorna a hora atual no formato "hh:mm:ss"
+function getCurrentTime() {
+    const date = new Date();
     return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
 }
 
-// Função que retorna a data atul (dd/mm/aaaa)
-function getCurrentDate(){
+// Retorna a data atual no formato "dd/mm/aaaa"
+function getCurrentDate() {
     const date = new Date();
-
     let mes = date.getMonth() + 1;
-
     return String(date.getDate()).padStart(2, '0') + "/" + String(mes).padStart(2, '0') + "/" + date.getFullYear();
 }
 
-// Função que retorna o dia da semana (De domingo a sábado / 0 - 6)
-function getWeekDay(){
+// Retorna o dia da semana como uma string (domingo a sábado)
+function getWeekDay() {
     const date = new Date();
-
     const dia = date.getDay();
     const semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
     return semana[dia];
 }
 
-// Chamada da função "updateContentHour" para atualizar instantânemante o horário no site
+// Atualiza a hora no site instantaneamente
 updateContentHour();
 
-// Função que atuliza a função "updateContentHour" a cada 1000 milisegundos
+// Atualiza a hora a cada segundo
 setInterval(updateContentHour, 1000);
 
+// Obtém o dialog do ponto anterior e os botões para registrar e fechar
 const pontoAnterior = document.getElementById("ponto-anterior");
-
 const btnRegistrarAnterior = document.getElementById("btn-registrar-anterior");
-
 const btnDialogFecharAnt = document.getElementById("dialog-fechar-anterior");
 
-btnRegistrarAnterior.addEventListener("click",() =>{
+// Abre o dialog do ponto anterior ao clicar no botão
+btnRegistrarAnterior.addEventListener("click", () => {
     pontoAnterior.showModal();
 });
-
-btnDialogFecharAnt.addEventListener("click", ()=>{
-    pontoAnterior.close();
-})
 
 // ( ) A fazer, atualizar a data e o dia da semana se o usuário bater o ponto meia noite
 // (X) A fazer 2, usar <dialog> para criar um popup quando se é clicado no botão "Registrar ponto"
